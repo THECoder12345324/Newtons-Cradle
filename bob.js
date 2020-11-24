95b8890e3f8f09a47482c623455b8ebb5b09491c
THECoder12345324 <sanjeevsharma.ait@gmail.com>
2020-11-24 13:15:15 -0500

    Fixed the movements of the Bobs to make it look more like a newton's cradle and added the color-changing background

--- bob.js
+++ bob.js
@@ -2,9 +2,9 @@ class Bob{
     constructor(x, y, r) {
         var options = {
             isStatic: false,
-            restitution: 0.3,
-            friction: 0.5,
-            density: 0.4
+            restitution: 1,
+            friction: 0,
+            density: 1
         }
         this.width = r * 2;
         this.body = Matter.Bodies.circle(x, y, r, options);
@@ -14,6 +14,6 @@ class Bob{
         var pos = this.body.position
         ellipseMode(CENTER);
         fill("white");
-        ellipse(pos.x, pos.y, this.width);
+        ellipse(pos.x, pos.y, this.width, this.width);
     }
 }
